3286abff62ef72c788fbf85672f20d5e365c565a
coppolam <m.coppola@tudelft.nl>
2018-06-05 17:19:57 +0200

    updated gitignore

--- sw/settings.h
+++ sw/settings.h
@@ -11,16 +11,16 @@
 
 #ifndef CONTROLLER
 // #define CONTROLLER Controller_Cartesian
-#define CONTROLLER Controller_Bearing_Shape
-// #define CONTROLLER Controller_Aggregate
+// #define CONTROLLER Controller_Bearing_Shape
+#define CONTROLLER Controller_Aggregate
 // #define CONTROLLER Controller_Bearing
 // #define CONTROLLER Controller_Keep_Aggregate
 #endif
 
 #ifndef CONTROLLER_INCLUDE
 // #define CONTROLLER_INCLUDE "controller_cartesian.h"
-#define CONTROLLER_INCLUDE "controller_bearing_shape.h"
-// #define CONTROLLER_INCLUDE "controller_aggregate.h"
+// #define CONTROLLER_INCLUDE "controller_bearing_shape.h"
+#define CONTROLLER_INCLUDE "controller_aggregate.h"
 // #define CONTROLLER_INCLUDE "controller_bearing.h"
 // #define CONTROLLER_INCLUDE "controller_keep_aggregate.h"
 #endif
