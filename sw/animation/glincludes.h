1a87818513b989fc3f66e169c336b6526564f5db
coppolam <m.coppola@tudelft.nl>
2018-06-12 15:30:48 +0200

    each file now has its own object

--- sw/animation/glincludes.h
+++ sw/animation/glincludes.h
@@ -2,11 +2,12 @@
 #define GLINCLUDES_H
 // This header file contains all the includes needed for the animation using openGL.
 
-#include <GL/glut.h>
-#include <GL/glu.h>
-#include <GL/glx.h>    // This includes the necessary X headers
-#include <GL/gl.h>
-#include <GLFW/glfw3.h>  // Install libglfw3 libglfw3-dev libglfw-dev for this to work
-#include <GL/freeglut.h>
-
+extern "C" {
+    #include <GL/glut.h>
+    #include <GL/glu.h>
+    #include <GL/glx.h>    // This includes the necessary X headers
+    #include <GL/gl.h>
+    #include <GLFW/glfw3.h>  // Install libglfw3 libglfw3-dev libglfw-dev for this to work
+    #include <GL/freeglut.h>
+}
 #endif /*GLINCLUDES_H*/
