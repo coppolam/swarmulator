f8ab080981b11a745925f883b6ff2dbf0d97263f
coppolam <m.coppola@tudelft.nl>
2018-09-21 11:56:43 +0200

    [scripts] made script to make new agent

---
+++ scripts/make_new_agent.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#echo The script that creates the header file (*.h) and source (*.cpp).
+
+if [ "$1" = "" ]; then
+	echo "Please specify the name of the agent.\n";
+	exit 1;
+fi
+
+folder=../sw/simulation/agents;
+
+if [ -f $folder/$1".h" ] || [ -f $folder/$1".cpp" ]; then
+	echo "An agent with this name already exists. Do you want to over write it? [yes]";
+	read option;
+	if [ "$option" != "yes" ]; then
+		exit 0;
+	fi
+fi
+
+str=$1
+c=`echo $str | awk '{print toupper($0)}'`
+
+awk -vn=$1 -vN=$c -vp=$folder 'BEGIN{
+	hN = p"/"n".h";
+	print "Creating header: "n".h ...";
+	print "#ifndef "N"_H" > hN;
+	print "#define "N"_H\n" > hN;
+	print "#include <vector>" > hN;
+	print "#include <stdio.h>" > hN;
+	print "#include <iostream>" > hN;
+	print "#include \"agent.h\"\n" > hN;
+	print "using namespace std;\n" > hN
+	print "class "n": public Agent" > hN;
+	print "{" > hN;
+	print "public:" > hN;
+	print "	"n"(int i, vector<float> state, float tstep);" > hN;
+	print "	void state_update();" > hN;
+	print "	void animation();" > hN;
+	print "};\n" > hN;
+	print "#endif /*"N"_H*/" >> hN;
+
+	cN = p"/"n".cpp";
+	print "Creating file implementation: "n".cpp ...";
+	print "#include \""n".h\"" > cN;
+	print "#include \"draw.h\"\n" > cN;
+	print n"::"n"(int i, vector<float> state, float tstep)\n{" >> cN;
+	print "  state = s;\n  ID = i;\n  dt = tstep;\n  orientation = 0.0;" > cN;
+	print "}\n" >> cN;
+	print "void "n"::state_update()\n{ \n}\n" > cN;
+	print "void "n"::animation()()\n{ \n}" > cN;
+}'
+
+exit 0;
