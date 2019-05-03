4f253582e596dea86589fa22293b9554cfbe8865
coppolam <m.coppola@tudelft.nl>
2019-05-03 19:15:28 +0200

    settings

--- sw/settings.h
+++ sw/settings.h
@@ -11,7 +11,7 @@
 // #define REMAIN_CONNECTED // Check that the swarm remains connected (only if logging)
 #define CHECK_HAPPY // Check whether the global goal is completed  (only if logging)
 
-// #define ARENAWALLS 20
+// #define ARENAWALLS 30
 
 /***** Select the controller to be used by uncommenting it ****/
 #define CONTROLLER Controller_Cartesian   // Basic x-y controller
