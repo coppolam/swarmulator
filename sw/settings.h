cd6615d5e0b5cd351970bba2b69ebec424136980
Mario Coppola <m.coppola@tudelft.nl>
2020-03-18 23:48:11 +0100

    switched default mode

--- sw/settings.h
+++ sw/settings.h
@@ -12,21 +12,21 @@
 // #define CHECK_HAPPY // Check whether the global goal is completed  (only if logging)
 
 /***** Select the controller to be used by uncommenting it ****/
-// #define CONTROLLER controller_aggregation // Aggregation controller (needs walls!)
-// #define ARENAWALLS 30 // Define the area for the aggreagation controller!
+#define CONTROLLER controller_aggregation // Aggregation controller (needs walls!)
+#define ARENAWALLS 30 // Define the area for the aggreagation controller!
 
 // #define CONTROLLER controller_lattice  // Basic lattice controller
 // #define CONTROLLER controller_pattern  // Controller with Qf
 
-#define CONTROLLER ndi_follower  // NDI follower, needs AGENT particle_oriented, to be defined below!
-#define COMMAND_LOCAL 1  // use COMMAND_LOCAL for local commands
+// #define CONTROLLER ndi_follower  // NDI follower, needs AGENT particle_oriented, to be defined below!
+// #define COMMAND_LOCAL 1  // use COMMAND_LOCAL for local commands
 
 // #define CONTROLLER behavior_tree // Controller with behvaior_tree_aggregation
 // #define CONTROLLER behavior_tree_wheeled // Controller with Qf
 
 /***** Select the agent type that you want to use by uncommenting it****/
-// #define AGENT particle    // Accelerated particles
-#define AGENT particle_oriented    // Accelerated particle with orientation
+#define AGENT particle    // Accelerated particles
+// #define AGENT particle_oriented    // Accelerated particle with orientation
 // #define AGENT wheeled    // Wheeled robot
 
 /***** Noise in relative sensing *****/
